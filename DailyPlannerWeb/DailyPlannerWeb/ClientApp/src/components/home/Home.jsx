b439575a73e8c9f551eddee23182586207a1f3b6
Валентин Борнашов <shipverty@vk.com>
2024-02-07 22:57:34 +0700

    feature(Home.jsx): изменен текст в заголовке

--- DailyPlannerWeb/DailyPlannerWeb/ClientApp/src/components/home/Home.jsx
+++ DailyPlannerWeb/DailyPlannerWeb/ClientApp/src/components/home/Home.jsx
@@ -5,7 +5,7 @@ class Home extends Component {
     render() {
         return (
             <div>
-                <h1>Hello World!</h1>
+                <h1>Hello World! oaoaoaoa</h1>
             </div>
         );
     }

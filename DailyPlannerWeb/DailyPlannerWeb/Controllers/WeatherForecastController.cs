9315f6e6089cb77fbe1fca85ca63a020a981f35a
Валентин Борнашов <shipverty@vk.com>
2024-02-07 23:20:40 +0700

    feature(asds):dsada

--- DailyPlannerWeb/DailyPlannerWeb/Controllers/WeatherForecastController.cs
+++ DailyPlannerWeb/DailyPlannerWeb/Controllers/WeatherForecastController.cs
@@ -9,7 +9,7 @@ public class WeatherForecastController : ControllerBase
     private static readonly string[] Summaries = new[]
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering",
-        "Scorching"
+        "Scorcasdahing"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
